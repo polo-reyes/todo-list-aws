131b164fdf634b6db6ad1aea13640bb096dcb29f
polo-reyes <reyescuarenta@gmail.com>
2022-06-08 16:40:50 -0700

    Revert "Added None to get Table call to raise exception"
    This reverts commit f23b9bd4cae83b846a32062928ca8c3d11cd97f7.

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -77,7 +77,9 @@ class TestDatabaseFunctions(unittest.TestCase):
         print('Table name:' + self.table.name)
         try:
             from src.todoList import get_table            
-            assert get_table(None) is None
+            self.assertRaises(Exception, get_table(None))
+            self.assertRaises(Exception, get_table("Polo"))
+            self.assertIn(get_table("Polo"), self.table.name)
         except ClientError as e:
             print(e.response['Error']['Message'])        
         print ('End: test_table_exists_error')
