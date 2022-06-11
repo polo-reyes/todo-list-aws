1ed1630e61c3c4acfcc23e77e2dd27a79c4f69b8
polo-reyes <reyescuarenta@gmail.com>
2022-06-10 23:47:46 -0700

    removed islatin function from translate.py

--- src/translate.py
+++ src/translate.py
@@ -6,21 +6,10 @@ import todoList
 import boto3
 
 
-def isLatin(textoTraducido):
-    try:
-        textoTraducido.encode(encoding='utf-8').decode('ascii')
-    except UnicodeDecodeError:
-        return False
-    else:
-        return True
-
-
 def translate(event, context):
-    #call to translate
     translate = boto3.client(service_name='translate',
                              region_name='us-east-1',
                              use_ssl=True)
-    #catching the arguments from event
     if (event['pathParameters']['language']! = ''):
         langTarget = event['pathParameters']['language']
     else:
@@ -29,15 +18,11 @@ def translate(event, context):
     item = todoList.get_item(event['pathParameters']['id'])
     print(item)
     textoAObtenerPorID = item['text']
-    #translating the text gotten by the get_item call
     valor_traducido = translate.translate_text(Text=textoAObtenerPorID,
                                                SourceLanguageCode="auto",
                                                TargetLanguageCode=langTarget)
-    print(valor_traducido.get('TranslatedText'))
-    #checking if translated text is latin-based
-    isLatin(valor_traducido.get('TranslatedText'))
+    print(valor_traducido.get('TranslatedText'))  
 
-    # create a response
     response = {
         "statusCode": 200,
         "body": json.dumps(valor_traducido,
